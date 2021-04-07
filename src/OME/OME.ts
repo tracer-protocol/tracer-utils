070a6e8cd0f057b55a6e86159f50275cf618956c
Raymond Mogg <raymondmogg@gmail.com>
2021-04-07 10:11:42 +1000

    Update ome lib (#4)
    * a
    
    * pass in ome address
    
    * Fix ome order type and path for new OME spec
    
    * update readme and gitignore
    
    * add OME getter helpers
    
    * update signing return type. Update ome calls
    
    * remove serialisation
    
    * remove lowercase, use checksum addresses. Change orderToOmeOrder types
    
    * simplify order to ome order
    
    * fix export issue
    
    Co-authored-by: tetther1122 <tetthera@gmail.com>

--- src/OME/OME.ts
+++ src/OME/OME.ts
@@ -1,23 +1,33 @@
 const fetch = require("node-fetch")
-import { OrderData, Signature, SignedOrder } from '../Types/types';
+//import fetch from "node-fetch"
+import { OrderData, SignedOrder } from '../Types/types';
 
-const orderToOMEOrder:(web3: any,  order: OrderData, sig: Signature) => SignedOrder = (web3, order, sig) => {
+//serialise an order for the OME
+const orderToOMEOrder:(web3: any, signedOrder: { order: OrderData, sigR: String, sigS: String, sigV: number }) => SignedOrder = (web3, signedOrder) => {
     let omeOrder = {
         id: "123",
-        address: order.user,
-        market: order.targetTracer,
-        side: order.side ? "Bid" : "Ask",
-        price: order.price,
-        amount: order.amount,
-        expiration: order.expiration,
-        signed_data: web3.utils.hexToBytes("0x" + sig.sigR.substring(2) + sig.sigS.substring(2) + sig.sigV.toString(16)),
-        nonce: web3.utils.toHex(order.nonce)
+        address: web3.utils.toChecksumAddress(signedOrder.order.user),
+        market: web3.utils.toChecksumAddress(signedOrder.order.targetTracer),
+        side: signedOrder.order.side ? "Bid" : "Ask",
+        price: signedOrder.order.price,
+        amount: signedOrder.order.amount,
+        expiration: signedOrder.order.expiration,
+        signed_data: web3.utils.hexToBytes("0x" + signedOrder.sigR.substring(2) + signedOrder.sigS.substring(2) + signedOrder.sigV.toString(16)),
+        nonce: web3.utils.toHex(signedOrder.order.nonce)
     }
     return omeOrder
 }
 
+/**
+ * Converts a market address from its ETH address form 0xaBc to
+ * its ome form aBc
+ */
+const omefy = (market: string) => {
+    return market.slice(2)
+}
 
 const createMarket = async (market: string, omeAddress: string) => {
+    // Note: The ome accepts a market of the form 0x here as it is in data, not the path
     return fetch(`${omeAddress}/book`, { method: "POST", body: JSON.stringify({market: market}), headers: { 'Content-Type': 'application/json' } })
 }
 
@@ -26,11 +36,11 @@ const getMarkets = async(omeAddress: string) => {
 }
 
 const getOrders = async(market: string, omeAddress: string) => {
-    return fetch(`${omeAddress}/book/${market}`, { method: "GET", headers: { 'Content-Type': 'application/json' } })
+    return fetch(`${omeAddress}/book/${omefy(market)}`, { method: "GET", headers: { 'Content-Type': 'application/json' } })
 }
 
 const submitOrder = async (market: string, order: SignedOrder, omeAddress: string) => {
-    return fetch(`${omeAddress}/book/${market}/order`, { method: "POST", body: JSON.stringify(order), headers: { 'Content-Type': 'application/json' } })
+    return fetch(`${omeAddress}/book/${omefy(market)}/order`, { method: "POST", body: JSON.stringify(order), headers: { 'Content-Type': 'application/json' } })
 }
 
 export {
