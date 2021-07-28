c0819cedac6ae52c7eb4640215233242a51cad9b
Chris Martin <chris@crunchycloud.io>
2021-07-28 13:46:32 +1000

    [constants] add api codes

---
+++ src/Constants/index.ts
@@ -0,0 +1,21 @@
+const API_CODES = {
+  INVALID_SIGNATURE: 'invalid_signature',
+  UNDER_MARGIN: 'under_margin',
+  INVALID_CREATED_TIMESTAMP: 'invalid_created_timestamp',
+  INVALID_EXPIRY_TIMESTAMP: 'invalid_expiry_timestamp',
+  NOT_WHITELISTED: 'not_whitelisted',
+  ORDER_PLACED: 'order_placed',
+  ORDER_FULLY_MATCHED: 'order_fully_matched',
+  ORDER_PARTIALLY_MATCHED: 'order_partially_matched',
+  CANCELLED: 'cancelled',
+  BOOK_CREATED: 'book_created',
+  BOOK_EXISTS: 'book_exists',
+  INVALID_MAKER: 'invalid_maker',
+  INVALID_TAKER: 'invalid_taker',
+  MARKET_MISMATCH: 'market_mismatch',
+  PRICE_NOT_CROSSED: 'price_not_crossed'
+}
+
+module.exports = {
+  API_CODES
+}
